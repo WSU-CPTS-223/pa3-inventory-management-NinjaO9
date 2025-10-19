1707a23457af66f395366002b432992433bba2e1
NinjaO9 <davidmontesvilanova@gmail.com>
2025-10-19 15:04:08 -0700

    csv parsing functionality*

--- HeaderFiles/REPL.hpp
+++ HeaderFiles/REPL.hpp
@@ -1,9 +1,19 @@
 #include "libs.h"
+#include "../HeaderFiles/linkedlist.hpp"
+#include "../HeaderFiles/hashtable.hpp"
+#include "../HeaderFiles/productdata.hpp"
 
-void printHelp();
+
+void print_help();
 
 bool valid_command(string line);
 
-void eval_command(string line);
+void eval_command(string line, HashTable<string, ProductData>& product_table, HashTable<string, LinkedList<ProductData>*>& category_table);
 
 void bootstrap();
+
+void process_csv(HashTable<string, ProductData>& product_table, HashTable<string, LinkedList<ProductData>*>& category_table);
+
+ProductData parse_line(string line);
+
+void place_into_categories(const ProductData data, HashTable<string, LinkedList<ProductData>*>& category_table);
--- SourceFiles/REPL.cpp
+++ SourceFiles/REPL.cpp
@@ -1,6 +1,22 @@
 #include "../HeaderFiles/REPL.hpp"
+#include <exception>
 
-void printHelp()
+// Custom exception for coolness
+class FileNotFound : public exception
+{
+    public:
+
+    FileNotFound(const string& msg) : message(msg){};
+
+    const char* what() const noexcept override {return message.c_str();}
+
+    private:
+
+    const string message;
+};
+
+
+void print_help()
 {
     cout << "Supported list of commands: " << endl;
     cout << " 1. find <inventoryid> - Finds if the inventory exists. If exists, prints details. If not, prints 'Inventory not found'." << endl;
@@ -16,17 +32,18 @@ bool valid_command(string line)
            (line.rfind("listInventory") == 0);
 }
 
-void eval_command(string line)
+void eval_command(string line, HashTable<string, ProductData>& product_table, HashTable<string, LinkedList<ProductData>*>& category_table)
 {
     if (line == ":help")
     {
-        printHelp();
+        print_help();
     }
     // if line starts with find
     else if (line.rfind("find", 0) == 0)
     {
         // Look up the appropriate datastructure to find if the inventory exist
-        cout << "YET TO IMPLEMENT!" << endl;
+        //cout << "YET TO IMPLEMENT!" << endl;
+        cout << product_table["fca4a2170dbe85896ac3daff0a7f1e1b"] << endl;
     }
     // if line starts with listInventory
     else if (line.rfind("listInventory") == 0)
@@ -41,8 +58,84 @@ void bootstrap()
     cout << "\n Welcome to Amazon Inventory Query System" << endl;
     cout << " enter :quit to exit. or :help to list supported commands." << endl;
     cout << "\n> ";
+
     // TODO: Do all your bootstrap operations here
     // example: reading from CSV and initializing the data structures
     // Don't dump all code into this single function
     // use proper programming practices
+}
+
+void process_csv(HashTable<string, ProductData>& product_table, HashTable<string, LinkedList<ProductData>*>& category_table)
+{
+    ifstream infile = ifstream("ResourceFiles/marketing_sample_for_amazon_com-ecommerce__20200101_20200131__10k_data.csv");
+    //ifstream infile = ifstream("ResourceFiles/sample.csv");
+
+    if (!infile.is_open()) throw FileNotFound("File was not found. Check the path?");
+
+    ProductData prod_data = ProductData();
+    string line;
+    string attributes_list[28];
+    getline(infile, line); // get rid of discriptor line
+
+
+    while (!infile.eof())
+    {
+        getline(infile, line);
+        prod_data = parse_line(line);
+        //cout << prod_data.get_uniqid() << endl;
+        place_into_categories(prod_data, category_table);
+        product_table.insert(prod_data.get_uniqid(), prod_data);
+    }
+}
+
+// Modified code from the previous PA
+ProductData parse_line(string line)
+{
+
+    string::size_type cindex1 = 0, cindex2 = 0;
+    unsigned int index = UNIQ_ID;
+    string fields[28] = {};
+
+    while (index != LAST)
+    {
+        cindex2 = line.find(",", cindex1);
+        if (cindex2 == string::npos)
+            cindex2 = line.length() - 1;
+        fields[index] = line.substr(cindex1, cindex2 - cindex1);
+        cindex1 = cindex2 + 1;
+        index++;
+    }
+    return ProductData(fields);
+}
+
+void place_into_categories(const ProductData data, HashTable<string, LinkedList<ProductData>*>& category_table)
+{
+    if (data.get_categories() == "N/A") return;
+    LinkedList<string> categories = LinkedList<string>();
+    string::size_type cindex1 = 0, cindex2 = 0;
+    string line = data.get_categories();
+    string sub = "";
+
+    while (cindex2 < line.length())
+    {
+        cindex2 = line.find("|", cindex1);
+        if (cindex2 == string::npos)
+            cindex2 = line.length() + 1;
+        sub = line.substr(cindex1, (cindex2 - 1) - cindex1);
+        sub.erase(remove_if(sub.begin(), sub.end(), ::isspace), sub.end());
+        categories.push_back(sub);
+        cindex1 = cindex2 + 1;
+    }
+
+    for (auto it = categories.begin(); it != categories.end(); ++it)
+    {
+        if (category_table.find(*it))
+        {
+            category_table[*it]->push_back(data);
+        }
+        else
+        {
+            category_table.insert(*it, new LinkedList<ProductData>(data));
+        }
+    }
 }
